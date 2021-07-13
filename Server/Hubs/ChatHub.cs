6394dab76a2ef05a62caf7e8e3bfae8928b64416
Kleyton72 <jean_santos1@ucne.edu.do>
2021-07-13 19:40:47 -0400

    Add project files.

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblySignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
+        public async Task GetAllUsers()
+        {
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveUsers", Users._Users);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Users._Users.Add(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Users._Users.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Server/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblySignalRApp.Server
+{
+    public static class Users
+    {
+        public static List<string> _Users { get; set; } = new List<string>();
+    }
+}
